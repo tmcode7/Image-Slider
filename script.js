881395e2636e1340e1cb41714dc5a783bdf3dfd6
tmcode7 <mawuenatord7@gmail.com>
2022-04-13 22:55:09 +0000

    added script to html

---
+++ script.js
@@ -0,0 +1,14 @@
+
+      var swiper = new Swiper("swiper mySwiper", {
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        },
+      });
